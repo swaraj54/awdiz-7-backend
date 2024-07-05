89695239f0b1b215698c5535bd19113107080323
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2024-07-05 21:19:47 +0530

    Updates

---
+++ controllers/auth.controllers.js
@@ -0,0 +1,7 @@
+export const Login = (req, res) => {
+    res.send("Login completed.")
+}
+
+export const Register = (req, res) => {
+    res.send("Register completed.")
+}
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const express = require("express");
+import express from 'express'
+import { Login, Register } from './controllers/auth.controllers.js';
 const app = express();
+app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.send("Hello Everyone kjabduklamdnhaw.");
+app.post("/", function (req, res) {
+  const { name, email, password } = req.body.userData;
+  console.log(name, email, password)
+  if (name && email && password) {
+    res.send("Data recevied.");
+  } else {
+    res.send("All fields are mandatory.")
+  }
+});
+
+app.post('/register', Register)
+app.post('/login', Login)
+
+
+app.get("/hello", function (req, res) {
+  res.send("Hello.");
 });
 
 app.listen(3000);
