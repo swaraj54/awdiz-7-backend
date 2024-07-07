c4dca11be58980155ecd21963856be649e31bcfe
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2024-07-07 14:55:17 +0530

    Updates

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,34 @@
-export const Login = (req, res) => {
-    res.send("Login completed.")
-}
+import User from "../models/user.model.js";
 
-export const Register = (req, res) => {
-    res.send("Register completed.")
-}
+export const Login = (req, res) => {
+  res.send("Login completed.");
+};
+
+export const Register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body.userData;
+    if (!name || !email || !password) {
+      return res.json({ success: false, error: "All fields are required." });
+    }
+
+    // check to check email is exists
+    // incrypt the password then store it in mongodb
+
+    const newUser = new User({
+      name: name,
+      email: email,
+      password,
+    });
+
+    const responseFromDb = await newUser.save();
+
+    return res.json({
+      success: true,
+      responseFromDb,
+      message: "Registeration Successfull.",
+    });
+  } catch (error) {
+    console.log(error, "error");
+    return res.json({ error: error, success: false });
+  }
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import AllRoutes from "./routes/index.js";
+import mongoose from "mongoose";
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -11,7 +12,10 @@ app.get("/", function (req, res) {
 
 app.use("/api/v1", AllRoutes);
 
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB connected."));
+
 app.listen(process.env.PORT_NUMBER, () => {
   console.log(`Server is running on port ${process.env.PORT_NUMBER}.`);
 });
-
